955cc057b7e090003e434fa95388babfae8f748e
meitarbasson <meitarbass@gmail.com>
2022-06-19 23:12:02 +0300

    Try, catch for open file.

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -1,19 +1,29 @@
 #include "Mtmchkin.h"
 
 Mtmchkin::Mtmchkin(const std::string fileName) {
+    std::string name = "/inputs/deck.txt";
+
     std::ifstream cardFile;
-    cardFile.open(fileName);
+    try {
+        cardFile.open(name);
+    } catch (const std::ifstream::failure& e) {
+        // TODO: Exception
+    }
+
+    while(!cardFile.eof()) {
+
+    }
 
     //TODO: Create Deck - from file
 
     //TODO: Create Player queue - from user
-
 }
 
 void Mtmchkin::playRound() {
     m_roundNumber++;
 
     //TODO: Iterate over player_queue
+
 }
 
 void Mtmchkin::printLeaderBoard() const {
